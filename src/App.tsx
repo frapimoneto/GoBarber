8c02cf2b5e106224d2b97b3f0e96c58020a3125d
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2020-08-12 17:31:27 -0300

    signIn and signUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SignUp />
     <GlobalStyle />
   </>
 )
