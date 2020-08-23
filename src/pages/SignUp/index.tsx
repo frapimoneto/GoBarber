067109f6c06109616bc5c55db346dbac12dded8b
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2020-08-23 01:53:53 -0300

    unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,29 +9,35 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object):void {
+    console.log(data);
+  }
 
-      <form>
-        <h1>Faça seu cadastro</h1>
-        <Input name="name" icon={FiUser} placeholder="Nome" />
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-        <Input name="email" icon={FiMail} placeholder="Email" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
 
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+          <Input name="email" icon={FiMail} placeholder="Email" />
 
-        <Button type="submit">Cadastrar</Button>
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-      </form>
-      <a href="new">
-        <FiArrowLeft />
+          <Button type="submit">Cadastrar</Button>
+
+        </Form>
+        <a href="new">
+          <FiArrowLeft />
           Voltar para logon
         </a>
-    </Content>
-  </Container>
-);
+      </Content>
+    </Container>
+  )
+};
 
 export default SignUp;
